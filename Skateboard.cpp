8396ddc22d53ef063d8bc97f03266204b31d2608
Usher-j <56136040+Usher-j@users.noreply.github.com>
2019-10-04 15:31:42 -0400

    Completed Skateboard.cpp
    Completed the implementation of the Skateboard Vehicle Class

---
+++ Skateboard.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Jared Usher on 10/4/19.
+//
+
+#include "Skateboard.h"
+#include <cstdlib>
+
+Skateboard::Skateboard(string brand, string model) {
+    setBrand(brand);
+    setModel(model);
+}
+
+Skateboard::~Skateboard() = default;
+
+double Bicycle::mileageEstimate(double time) {
+    double mileage;
+    
+	if (time > 25 && time < 250)
+	{
+		mileage = (((rand()%5+1)/10) + (rand()%3+1)/3) * time;
+	}
+	else 
+	{
+		mileage = ((rand()%5+1)/10) * time;
+	}
+	
+    return mileage;
+}
+
+string Skateboard::toString() {
+    string s = "-> Skateboard\n\t";
+    return "-> Skateboard\n" + Vehicle::toString();
+}
