f99fc7005e5fa5cdc9e6a5ec5be0d978b86d6e45
Usher-j <56136040+Usher-j@users.noreply.github.com>
2019-10-04 15:18:59 -0400

    Partially Implemented Skateboard Class
    Created Skateboard.cpp and Skateboard.h
    Completed the Skateboard header file

---
+++ Skateboard.h
@@ -0,0 +1,24 @@
+//
+// Created by Jared USher on 10/4/19.
+//
+
+#ifndef DRIVINGSIMULATOR_SKATEBOARD_H
+#define DRIVINGSIMULATOR_SKATEBOARD_H
+
+#include "Vehicle.h"
+
+class Skateboard : public Vehicle {
+
+private:
+
+public:
+    explicit Skateboard(string brand, string model);
+
+    virtual ~Skateboard();
+    virtual double mileageEstimate(double time);
+
+    virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_SKATEBOARD_H
